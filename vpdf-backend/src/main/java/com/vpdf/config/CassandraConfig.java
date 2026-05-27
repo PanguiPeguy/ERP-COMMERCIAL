45e38eea5545e2d743820628ec81a642a7db5f04
PanguiPeguy <peguy.pangui@gmail.com>
2026-05-27 16:22:30 +0100

    correction d'une erreur sur cassandra config

--- vpdf-backend/src/main/java/com/vpdf/config/CassandraConfig.java
+++ vpdf-backend/src/main/java/com/vpdf/config/CassandraConfig.java
@@ -4,11 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
-import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
-import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
-
-import java.util.List;
 
 @Configuration
 @EnableCassandraRepositories(basePackages = "com.vpdf.repository")
@@ -25,6 +21,9 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Value("${spring.cassandra.local-datacenter:datacenter1}")
     private String localDatacenter;
+
+    @Value("${spring.cassandra.schema-action:NONE}")
+    private SchemaAction schemaAction;
 
     @Override
     protected String getKeyspaceName() {
@@ -48,22 +47,11 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Override
     public SchemaAction getSchemaAction() {
-        return SchemaAction.CREATE_IF_NOT_EXISTS;
+        return schemaAction;
     }
 
     @Override
-    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
-        return List.of(
-            CreateKeyspaceSpecification
-                .createKeyspace(keyspaceName)
-                .ifNotExists()
-                .with(KeyspaceOption.DURABLE_WRITES, true)
-                .withSimpleReplication(3L)
-        );
-    }
-
-    @Override
-    protected String[] getEntityBasePackages() {
+    public String[] getEntityBasePackages() {
         return new String[]{"com.vpdf.model"};
     }
 }
